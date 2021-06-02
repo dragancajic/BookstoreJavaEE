2d9eb1906654f3565fb2fd218f0aa03a88ced81e
Dragan Cajic <cajic_dragan@yahoo.com>
2021-06-02 22:49:28 +0200

    complete rest service

--- bookstore-back/src/main/java/org/eu5/learn_pisio/bookstore/rest/BookEndpoint.java
+++ bookstore-back/src/main/java/org/eu5/learn_pisio/bookstore/rest/BookEndpoint.java
@@ -1,4 +1,56 @@
 package org.eu5.learn_pisio.bookstore.rest;
 
+import org.eu5.learn_pisio.bookstore.model.Book;
+import org.eu5.learn_pisio.bookstore.repository.BookRepository;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+@Path("/books")
 public class BookEndpoint {
+	
+	@Inject
+	private BookRepository bookRepository;
+	
+	public Book getBook(Long id) {
+		return bookRepository.find(id);
+	}
+	
+	public Book createBook(Book book) {
+		return bookRepository.create(book);
+	}
+	
+	public void deleteBook(Long id) {
+		bookRepository.delete(id);
+	}
+	
+	@GET
+	@Produces(APPLICATION_JSON)
+	public Response getBooks() {
+		List<Book> books = bookRepository.findAll();
+		
+		if (books.size() == 0) {
+			return Response.noContent().build();
+		}
+		
+		return Response.ok(books).build();
+	}
+	
+	@GET
+	@Path("/count")
+	public Response countBooks() {
+		Long numberOfBooks = bookRepository.countAll();
+		
+		if (numberOfBooks == 0) { // if database is empty
+			return Response.noContent().build();
+		}
+		
+		return Response.ok(numberOfBooks).build();
+	}
 }
--- bookstore-back/src/main/java/org/eu5/learn_pisio/bookstore/rest/JAXRSConfiguration.java
+++ bookstore-back/src/main/java/org/eu5/learn_pisio/bookstore/rest/JAXRSConfiguration.java
@@ -1,4 +1,9 @@
 package org.eu5.learn_pisio.bookstore.rest;
 
-public class JAXRSConfiguration {
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+// Everything under API ("api") is a JAX-RS endpoint
+@ApplicationPath("api") // arbitrary string -- "api"
+public class JAXRSConfiguration extends Application {
 }
